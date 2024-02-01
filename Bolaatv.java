93d8e2fca34e207aa2e916f0dd7e44430afb7019
IsaqueNunes05 <isaquepontes.nunes@gmail.com>
2024-02-01 14:07:58 -0300

    bolas

--- Bolaatv.java
+++ Bolaatv.java
@@ -1,15 +1,11 @@
 
-// Definindo a classe "Pessoa"
 class Bola {
-    // Atributos da classe
-    private String cor;   // Atributo privado para encapsulamento
+    private String cor;
 
-    // Método construtor da classe para inicializar os atributos
     Bola(String cor) {
         this.cor = cor;
     }
     
-    // Método getter para obter a idade da pessoa
     public String getCor() {
         return cor;
     }
@@ -17,16 +13,12 @@ class Bola {
     public void setCor(String cor){
         this.cor = cor;
     }
-
-
 }
 
-// Classe principal que contém o método main
 public class Bolaatv{
     public static void main(String[] args) {
-        // Criando um objeto da classe Pessoa chamado "pessoa1"
         Bola bola1 = new Bola("Azul");
-        System.out.println("Cor: " + bola1.getCor()); // Obtém e imprime a idade da pessoa
+        System.out.println("Cor: " + bola1.getCor()); 
 
         bola1.setCor("Roxo");
         System.out.println("Cor: " + bola1.getCor());
