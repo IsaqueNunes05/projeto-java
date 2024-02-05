c943c9bc79eea2351471c36c2651d3ab7dd9d1b3
IsaqueNunes05 <158211611+IsaqueNunes05@users.noreply.github.com>
2024-02-05 17:11:54 -0300

    Add files via upload

---
+++ Cachorroatv.java
@@ -0,0 +1,13 @@
+class Cachorro {
+    
+    public void Latir(){
+        System.out.println("Au Au");
+    }
+}
+
+public class Cachorroatv{
+    public static void main(String[] args) {
+        Cachorro Cachorro1 = new Cachorro();
+        Cachorro1.Latir();
+    }
+}
---
+++ Carroatv.java
@@ -0,0 +1,14 @@
+class Automovel{
+    
+    public void Ligar(){
+        System.out.println("Vrum Vrum");
+    }
+
+}
+
+public class Carroatv{
+    public static void main(String[] args) {
+        Automovel Carro1 = new Automovel();
+        Carro1.Ligar();
+    }
+}
